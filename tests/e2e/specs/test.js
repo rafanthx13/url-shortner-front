bb1968940435a497391dd104d1c40c9a8cb0918a
RafaPCHome <rafaassis15@gmail.com>
2021-12-21 14:15:26 -0300

    RandomWindowsCommit

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -6,6 +6,5 @@ describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('http://localhost:8080/')
     cy.contains('h1', 'Quasar URL Shortener')
-    cy.get('q-btn').type('alfa')
   })
 })
